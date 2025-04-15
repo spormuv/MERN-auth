99d4168c4219d55ca34bd73380ce67c86289f6a2
Mike <spormuv@gmail.com>
2025-04-16 01:36:49 +0300

    feat: connect local MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,18 @@
+import 'dotenv/config';
 import express from 'express';
+import connectToDatabase from './config/db';
 
 const app = express();
 
-app.listen(4004, () => {
+// app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'healthy',
+  });
+});
+
+app.listen(4004, async () => {
   console.log('Server running on port 4004 in development environment');
+  await connectToDatabase();
 });
