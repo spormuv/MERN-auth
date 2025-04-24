b2222e3222a1d9b11f58e9070007b9f3c13a1f58
Mike <spormuv@gmail.com>
2025-04-25 01:48:27 +0300

    feat: add providers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+
+export const Home = () => {
+  return <div>Home</div>;
+};
+
 function App() {
-  return <>Test</>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import queryClient from './config/queryClient.js';
+import theme from './theme/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools position='bottom-right' initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
